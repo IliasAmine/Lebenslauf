651111b02206b2fa75639c1693267981a85f4cf9
IliasAmine <ilias.amine@stud.hs-bochum.de>
2024-08-22 15:30:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ document.addEventListener('DOMContentLoaded', function() {
             if (entry.isIntersecting) {
                 entry.target.style.opacity = 1;
                 entry.target.style.transform = 'translateY(0)';
-                entry.target.style.transition = 'all 0.6s ease';
                 observer.unobserve(entry.target);
             }
         });
     });
 
     timelineItems.forEach(item => {
-        item.style.opacity = 0;
-        item.style.transform = 'translateY(20px)';
         observer.observe(item);
     });
 
